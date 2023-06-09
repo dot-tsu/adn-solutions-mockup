fa60617f2d6f9608104448eef8759491dbf4213f
Tsu <lucanahtsu@gmail.com>
2023-06-08 22:08:02 -0300

    replace smooth scrolling script for a simpler one

--- main.js
+++ main.js
@@ -7,13 +7,14 @@ window.addEventListener("scroll", function () {
 // Smooth Scrolling
 document.addEventListener("DOMContentLoaded", () => {
   const navLinks = document.querySelectorAll("nav a");
-  const duration = 1000; // 1000 milliseconds = 1 second
 
   navLinks.forEach((navLink) => {
     navLink.addEventListener("click", (event) => {
       event.preventDefault();
       const target = document.querySelector(navLink.getAttribute("href"));
-      scrollTo(target, duration);
+      target.scrollIntoView({
+        behavior: "smooth",
+      });
     });
   });
 
@@ -21,40 +22,9 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault();
     const delta = Math.sign(event.deltaY);
     const scrollPosition = window.scrollY;
-    const scrollAmount = delta * window.innerHeight;
-    scrollTo(scrollPosition + scrollAmount, duration);
+    window.scrollTo({
+      top: scrollPosition + delta * window.innerHeight,
+      behavior: "smooth",
+    });
   });
-
-  function scrollTo(target, duration) {
-    const startPosition = window.scrollY;
-    let targetPosition;
-
-    if (typeof target === "number") {
-      targetPosition = target;
-    } else {
-      targetPosition = target.getBoundingClientRect().top + window.scrollY;
-    }
-
-    const distance = targetPosition - startPosition;
-    let startTime = null;
-
-    function animation(currentTime) {
-      if (startTime === null) startTime = currentTime;
-      const elapsedTime = currentTime - startTime;
-      const scrollY = ease(elapsedTime, startPosition, distance, duration);
-      window.scrollTo(0, scrollY);
-      if (elapsedTime < duration) {
-        requestAnimationFrame(animation);
-      }
-    }
-
-    function ease(t, b, c, d) {
-      t /= d / 2;
-      if (t < 1) return (c / 2) * t * t + b;
-      t--;
-      return (-c / 2) * (t * (t - 2) - 1) + b;
-    }
-
-    requestAnimationFrame(animation);
-  }
 });

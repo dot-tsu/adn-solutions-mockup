bb994bed31d7dbbbb4777a9bd2b40709b4a2efc4
Tsu <lucanahtsu@gmail.com>
2023-06-12 14:44:34 -0300

    fix smooth scroll not working on some browsers

--- main.js
+++ main.js
@@ -5,26 +5,33 @@ window.addEventListener("scroll", function () {
 });
 
 // Smooth Scrolling
-document.addEventListener("DOMContentLoaded", () => {
-  const navLinks = document.querySelectorAll("nav a");
+// Get all the section elements
+const sections = document.querySelectorAll("section");
 
-  navLinks.forEach((navLink) => {
-    navLink.addEventListener("click", (event) => {
-      event.preventDefault();
-      const target = document.querySelector(navLink.getAttribute("href"));
-      target.scrollIntoView({
-        behavior: "smooth",
-      });
-    });
-  });
+// Function to handle the scroll event
+function handleScroll(event) {
+  event.preventDefault();
 
-  document.addEventListener("wheel", (event) => {
-    event.preventDefault();
-    const delta = Math.sign(event.deltaY);
-    const scrollPosition = window.scrollY;
-    window.scrollTo({
-      top: scrollPosition + delta * window.innerHeight,
+  const delta = event.wheelDelta || -event.detail;
+  const direction = delta > 0 ? -1 : 1;
+
+  let visibleSectionIndex = -1;
+  for (let i = 0; i < sections.length; i++) {
+    const rect = sections[i].getBoundingClientRect();
+    if (rect.top >= 0) {
+      visibleSectionIndex = i;
+      break;
+    }
+  }
+
+  const nextSectionIndex = visibleSectionIndex + direction;
+
+  if (nextSectionIndex >= 0 && nextSectionIndex < sections.length) {
+    sections[nextSectionIndex].scrollIntoView({
       behavior: "smooth",
     });
-  });
-});
+  }
+}
+
+document.addEventListener("wheel", handleScroll, { passive: false });
+document.addEventListener("DOMMouseScroll", handleScroll, { passive: false });
